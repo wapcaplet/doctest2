62de13b71dda57a824672c292b2e29b81b56e387
Ian Bicking <ianb@colorstudy.com>
2011-03-13 15:45:56 -0400

    Initial copy over of doctest to doctest2

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+version = '0.1'
+
+setup(name='doctest2',
+      version=version,
+      description="An updating of the venerable doctest standard library module",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='doctest testing',
+      author='Ian Bicking',
+      author_email='ianb@colorstudy.com',
+      url='',
+      license='PSF',
+      py_modules=['doctest2'],
+      )
