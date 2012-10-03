8dffca8d957a72e04472b3e94443911d5b4c289f
Eric Pierce <wapcaplet88@gmail.com>
2012-10-03 13:41:50 -0600

    Summarize each test file, include self-test

--- test.py
+++ test.py
@@ -10,12 +10,21 @@ test_files = [
 ]
 
 if __name__ == '__main__':
+    # self-test
+    print("=" * 30)
+    print("doctest2.py")
+    print("-" * 30)
+    failures, tests = doctest2.testmod(doctest2)
+    print("%d tests, %d failures" % (tests, failures))
+    print("=" * 30)
+    print("")
+
     for filename in test_files:
         print("=" * 30)
         print(filename)
+        print("-" * 30)
+        failures, tests = doctest2.testfile(filename, verbose=False)
+        print("%d tests, %d failures" % (tests, failures))
         print("=" * 30)
-
-        doctest2.testfile(filename, verbose=False)
-
         print("")
 
